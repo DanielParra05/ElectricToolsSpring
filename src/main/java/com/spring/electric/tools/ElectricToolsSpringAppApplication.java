c29bff6aa40bed27eeaec0f6b613e594f1174ea4
DanielParra <danielparra05@outlook.com>
2019-04-04 21:31:35 -0500

    Primer commit local

---
+++ src/main/java/com/spring/electric/tools/ElectricToolsSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.electric.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricToolsSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricToolsSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/electric/tools/ElectricToolsSpringAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.electric.tools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElectricToolsSpringAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
