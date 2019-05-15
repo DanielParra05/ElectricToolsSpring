5232208a9b85f319398b1283debd0e4bf57092dd
DanielParra <danielparra05@outlook.com>
2019-05-15 01:26:54 -0500

    Adicion de nuevos campos para la entidad Cliente

--- src/main/java/com/spring/electric/tools/models/entities/Cliente.java
+++ src/main/java/com/spring/electric/tools/models/entities/Cliente.java
@@ -7,16 +7,11 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 @Entity
 @Table(name="CLIENTE")
 public class Cliente implements Serializable{
@@ -30,18 +25,30 @@ public class Cliente implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="ID_CLIENTE")
 	private Long idCliente;
+	
 	@Column(name = "NOMBRE",nullable = false)
-	private String nombre;
+	private String nombre;	
+	
 	@Column(name = "APELLIDO")
 	private String apellido;
-	@Column(name ="EMAIL",nullable = false, unique = true)
-	private String email;
-	@OneToMany(mappedBy="cliente", cascade = CascadeType.ALL)
 	
+	@Column(name ="EMAIL",nullable = false, unique = true)
+	private String email;	
+	
+	@OneToMany(mappedBy="cliente", cascade = CascadeType.ALL)	
 	private List<Orden> listaOrdenes;
 	
 	@Column(name = "CREATE_AT")
 	private LocalDate createAt;
+	
+	@Column(name="CEDULA")
+	private String cedula;
+	
+	@Column(name="DIRECCION")
+	private String direccion;
+	
+	@Column(name="TELEFONOS")
+	private String telefonos;
 
 	public Long getId() {
 		return idCliente;
@@ -81,6 +88,46 @@ public class Cliente implements Serializable{
 
 	public void setCreateAt(LocalDate createAt) {
 		this.createAt = createAt;
+	}
+
+	public Long getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Long idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public List<Orden> getListaOrdenes() {
+		return listaOrdenes;
+	}
+
+	public void setListaOrdenes(List<Orden> listaOrdenes) {
+		this.listaOrdenes = listaOrdenes;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefonos() {
+		return telefonos;
+	}
+
+	public void setTelefonos(String telefonos) {
+		this.telefonos = telefonos;
 	}
 
 	
