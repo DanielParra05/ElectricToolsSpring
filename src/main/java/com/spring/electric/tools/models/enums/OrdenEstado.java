fffbf6262685cd2437fb2c9397761be92d1892cd
DanielParra <danielparra05@outlook.com>
2019-06-10 22:55:37 -0500

    Nueva clase3

---
+++ src/main/java/com/spring/electric/tools/models/enums/OrdenEstado.java
@@ -0,0 +1,21 @@
+package com.spring.electric.tools.models.enums;
+
+/**
+ * Clase para indicar el estado de una orden
+ * @author Jose Daniel Parra
+ *
+ */
+public enum OrdenEstado { 
+	EN_REPARACION("En Reparacion"),
+	DE_SALIDA("De Salida");
+	
+	private String valor;
+	
+	private OrdenEstado(String valor) {
+		this.valor = valor;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+}
